3e6098b66a4d1ad7a0d3a96b33752718371e42c7
malcolm <me@jgmalcolm.com>
2016-04-25 12:44:05 -0400

    inverthardcopy default

--- startup.m
+++ startup.m
@@ -4,8 +4,8 @@ format short
 colordef black
 dbstop if error
 set(0, 'DefaultFigureMenu', 'none', ...
-       'DefaultFigureColor', 'k')
-set(gcf, 'InvertHardcopy', 'off')
+       'DefaultFigureColor', 'k', ...
+       'DefaultFigureInvertHardcopy', 'on')
 
 p = which('startup');
 p = p(1:end-numel('startup.m'));
