f248fe741a474f52d77f575e384c90a41456e905
malcolm <me@jgmalcolm.com>
2016-05-17 12:34:51 -0400

    if only one variable, assume that

--- lib/loadcached.m
+++ lib/loadcached.m
@@ -20,4 +20,10 @@ function varargout = loadcached(file, varargin)
   for i = 1:nargin-1
     varargout{i} = data.(varargin{i});
   end
+
+  % if no inputs and .mat only had one variable, assume that is wanted
+  flds = fieldnames(data);
+  if nargin == 1 && numel(flds) == 1
+    varargout{1} = data.(flds{1});
+  end
 end
