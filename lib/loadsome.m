0d6d72bef26133be9d305cadbe59e8a68fb1836f
malcolm <me@jgmalcolm.com>
2016-06-01 04:47:37 -0400

    automatically return sole variable

--- lib/loadsome.m
+++ lib/loadsome.m
@@ -1,8 +1,14 @@
 function varargout = loadsome(file, varargin)
   fprintf('loading %s...\n', file)
-  s = load(file, varargin{:});
+  data = load(file, varargin{:});
 
-  for i = 1:nargout
-    varargout{i} = s.(varargin{i});
+  for i = 1:nargin-1
+    varargout{i} = data.(varargin{i});
+  end
+
+  % if no inputs and .mat only had one variable, assume that is wanted
+  flds = fieldnames(data);
+  if nargin == 1 && numel(flds) == 1
+    varargout{1} = data.(flds{1});
   end
 end
